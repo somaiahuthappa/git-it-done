0b0550321f326639e30fbbff38ab38a2505a8661
Som's Laptop <somaiahuthappa@gmail.com>
2022-02-20 23:46:06 -0500

    dynamically search users

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github API url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
